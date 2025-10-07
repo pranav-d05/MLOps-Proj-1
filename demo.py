e533370960294ccba7681e96cbf96b14dadbe927
pranav-d05 <pranavdhebe93@gmail.com>
2025-10-07 19:44:10 +0530

    Logging and exception added

---
+++ demo.py
@@ -0,0 +1,14 @@
+# below code is to check the logging config
+#  
+# --------------------------------------------------------------------------------
+
+# # below code is to check the exception config
+from src.logger import logging
+from src.exception import MyException
+import sys
+
+try:
+    a = 1+'Z'
+except Exception as e:
+    logging.info(e)
+    raise MyException(e, sys) from e
